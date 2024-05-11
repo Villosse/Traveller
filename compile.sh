ed2f27c87c882c2229fc21fe2bd93a8a3d92e28d
Lenny Chiadmi <lenny.chiadmi@gmail.com>
2024-05-11 18:40:16 +0200

    fix(compilation): compilation.sh

--- compile.sh
+++ compile.sh
@@ -2,16 +2,24 @@
 
 source "./.env"
 
+if [[ $PROJECT_NAME =~ [^a-zA-Z0-9_] ]]; then
+  echo "error : PROJECT_NAME contains a forbidden symbol"
+  exit 1
+fi
+
 new_line="set(EXECUTABLE_OUTPUT_PATH ${PROJECT_OUTPUT_DIR})"
 sed -i "11s,.*,$new_line," CMakeLists.txt
+
+new_line="add_executable(${PROJECT_NAME} src/main.cpp)"
+sed -i "8s,.*,$new_line," CMakeLists.txt
 
 mkdir -p "${PROJECT_OUTPUT_DIR}/build"
 
 cd "${PROJECT_OUTPUT_DIR}/build"
 
-#cmake ..
+cmake ..
 
-#make
+make
 
 if [ $? -eq 0 ]; then
     echo "Compilation successful. Executable is located at: ${PROJECT_OUTPUT_DIR}/${PROJECT_NAME}"
