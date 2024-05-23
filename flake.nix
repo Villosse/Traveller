610718f9a1c39a746afec527422f249539d17f52
Lenny Chiadmi-Delage <lenny.chiadmi-delage@epita.fr>
2024-05-23 12:35:18 +0200

    change base of project

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Simple flake to dev on our C++ Project";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs, ...}:
+  let system = "x86_64-linux";
+  pkgs = nixpkgs.legacyPackages.${system};
+  in
+  {
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = with pkgs; [
+      git
+      gcc
+      gnumake
+      cmake
+      qt5.qtbase
+    ];
+
+    shellHook = ''
+      clear
+      echo "Welcome to the flake :)"
+      source .env
+    '';
+
+  };
+  };
+}
