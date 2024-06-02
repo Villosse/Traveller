49f96e81256dffc1321d602beb6fcfd583e1e17f
Lenny Chiadmi <lenny.chiadmi@gmail.com>
2024-06-02 09:45:04 +0200

    refacto include and src

--- include/mainWindow.h
+++ include/mainWindow.h
@@ -4,6 +4,18 @@
 
 #define MAIN_WINDOW_H
 
+#include <SFML/Graphics.hpp>
+#include <vector>
+#include <iostream>
+#include <cmath>
+#include "Line.h"
 
+// Function to check if a point is within a rectangle
+bool isMouseOverButton(const sf::RectangleShape& button, const sf::Vector2f& mousePos);
+
+// Function to calculate the distance between two points
+float distanceBetweenPoints(const sf::Vector2f& p1, const sf::Vector2f& p2);
+
+void init();
 
 #endif
